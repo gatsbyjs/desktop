cff9db427034e3a0d57bea06e6adc885b5427f91
Matt Kane <matt@gatsbyjs.com>
2020-09-29 12:05:07 +0100

    Start background update

--- app/telemetry.ts
+++ app/telemetry.ts
@@ -1,10 +1,16 @@
-import telemetry from "gatsby-telemetry"
+import {
+  startBackgroundUpdate,
+  trackCli,
+  setDefaultComponentId,
+  setGatsbyCliVersion,
+  trackFeatureIsUsed,
+} from "gatsby-telemetry"
 import { app, ipcMain, IpcMainEvent } from "electron"
 import { ITelemetryTagsPayload } from "gatsby-telemetry/lib/telemetry"
 
-function trackFeatureIsUsed(event: IpcMainEvent, name: string): void {
+function telemetryTrackFeatureIsUsed(event: IpcMainEvent, name: string): void {
   console.log(`track feature is used`, name)
-  telemetry.trackFeatureIsUsed(name)
+  trackFeatureIsUsed(name)
 }
 
 function trackEvent(
@@ -13,12 +19,13 @@ function trackEvent(
   tags?: ITelemetryTagsPayload
 ): void {
   console.log(`track event`, input, tags)
-  telemetry.trackCli(input, tags)
+  trackCli(input, tags)
 }
 
 export function initializeTelemetry(): void {
-  telemetry.setDefaultComponentId(`gatsby-desktop`)
-  telemetry.setGatsbyCliVersion(app.getVersion())
-  ipcMain.on(`telemetry-trackFeatureIsUsed`, trackFeatureIsUsed)
+  setDefaultComponentId(`gatsby-desktop`)
+  setGatsbyCliVersion(app.getVersion())
+  startBackgroundUpdate()
+  ipcMain.on(`telemetry-trackFeatureIsUsed`, telemetryTrackFeatureIsUsed)
   ipcMain.on(`telemetry-trackEvent`, trackEvent)
 }
